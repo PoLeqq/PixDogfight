c7a218a4ba479bee43924a29c8183ecdd1217016
PoLeq <wiktorsolinski123@gmail.com>
2024-11-11 22:34:26 +0100

    GUI update - added new fields

--- core/src/main/java/pl/poleq/pixdogfight/screen/game/gui/GameGUI.java
+++ core/src/main/java/pl/poleq/pixdogfight/screen/game/gui/GameGUI.java
@@ -15,8 +15,11 @@ public class GameGUI {
     private final int WIDTH = Gdx.graphics.getWidth();
     private final int HEIGHT = Gdx.graphics.getHeight();
 
+    float gameTime = 0;
+
     public GameGUI(GameScreen screen) {
         this.screen = screen;
+        this.gameTime = 0;
 
 //        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("m5x7.ttf"));
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("victor-pixel.ttf"));
@@ -27,14 +30,20 @@ public class GameGUI {
         generator.dispose();
     }
 
-    public void render(SpriteBatch batch) {
+    public void render(SpriteBatch batch, float delta) {
+        gameTime += delta;
+
         Player player = screen.getPlayer();
         PlaneEquipment eq = player.getEquipment();
 
         int col = 0;
         font.draw(batch, String.format("PLT: %s",screen.getPlayer().getUsername()),getLabelX(0),getLabelY(col++));
-        font.draw(batch, String.format("THR: %d",player.getThrottle()),getLabelX(0),getLabelY(col++));
+        font.draw(batch, String.format("TIME: %s",getTimeStringified()),getLabelX(0),getLabelY(col++));
+        font.draw(batch, String.format("THR: %d%%",player.getThrottleTarget()),getLabelX(0),getLabelY(col++));
+        font.draw(batch, String.format("ETHR: %d%%",player.getThrottleTarget()),getLabelX(0),getLabelY(col++));
         font.draw(batch, String.format("SPD: %.0f",player.getSpeed()),getLabelX(0),getLabelY(col++));
+        font.draw(batch, String.format("THRUST: %.2f",player.getThrust()),getLabelX(0),getLabelY(col++));
+        font.draw(batch, String.format("ACC: %.2f",player.getAcceleration()),getLabelX(0),getLabelY(col++));
         font.draw(batch, String.format("ALT: %.0f",player.getLocation().getY()),getLabelX(0),getLabelY(col++));
 
         col+=2;
@@ -45,6 +54,7 @@ public class GameGUI {
         col+=2;
         font.draw(batch, String.format("X: %.0f",player.getLocation().getX()),getLabelX(0),getLabelY(col++));
         font.draw(batch, String.format("Y: %.0f",player.getLocation().getY()),getLabelX(0),getLabelY(col++));
+        font.draw(batch, String.format("R: %.2f",player.getLocation().getRotation()),getLabelX(0),getLabelY(col++));
     }
 
     private int getLabelY(int column) {
@@ -53,5 +63,22 @@ public class GameGUI {
 
     private int getLabelX(int row) {
         return 20 + row * 200;
+    }
+
+    private String getTimeStringified() {
+        int minutes = 0;
+        int seconds = 0;
+        float time = gameTime;
+
+        while(time >= 60) {
+            time -= 60;
+            minutes++;
+        }
+        while(time >= 1) {
+            time -= 1;
+            seconds++;
+        }
+
+        return String.format("%02d:%02d", minutes, seconds);
     }
 }
